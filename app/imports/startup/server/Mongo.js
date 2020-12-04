527b2ff3d3b30aaa4c2300e4901eed16386d88d2
Ian Kimura <kimurai@hawaii.edu>
2020-12-03 14:33:21 -1000

    Revert "Merge branch 'master' of https://github.com/Study-ext/study-ext"
    This reverts commit f77afe288042f9261a4e17e39ae1173553d83de4, reversing
    changes made to 2181f3f24f94a06cf96034a1cf22ea3d02f2c1b8.

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,25 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
-import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff.js';
-import { Contacts } from '../../api/contact/Contacts';
-import { Profiles } from '../../api/profile/Profiles';
 
 /* eslint-disable no-console */
-
-/** Define a user in the Meteor accounts package. This enables login. Username is the email address. */
-function createUser(email, password, role) {
-  console.log(`  Creating user ${email}.`);
-  const userID = Accounts.createUser({
-    username: email,
-    email: email,
-    password: password,
-  });
-  if (role === 'admin') {
-    Roles.createRole(role, { unlessExists: true });
-    Roles.addUsersToRoles(userID, 'admin');
-  }
-}
 
 /** Initialize the database with a default data document. */
 function addData(data) {
@@ -32,43 +14,5 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
-  }
-}
-
-/* for Contacts collection */
-function addContact(data) {
-  console.log(`  Adding: ${data.lastName} (${data.owner})`);
-  Contacts.collection.insert(data);
-}
-
-/** Initialize the collection if empty. */
-if (Contacts.collection.find().count() === 0) {
-  if (Meteor.settings.defaultContacts) {
-    console.log('Creating default contacts.');
-    Meteor.settings.defaultContacts.map(data => addContact(data));
-  }
-}
-
-/* for Profiles collection */
-// function addProfile(data) {
-//   console.log(`  Adding: ${data.name} (${data.owner})`);
-//   Profiles.insert(data);
-// }
-/** Defines a new user and associated profile. Error if user already exists. */
-function addProfile({ name, email, picture, currentClasses, takenClasses, bio, role }) {
-  console.log(`Defining profile ${email}`);
-  // Define the user in the Meteor accounts package.
-  createUser(email, role);
-  // Create the profile.
-  Profiles.insert({ name, email, picture, currentClasses, takenClasses, bio });
-}
-
-/** Initialize the DB if empty (no users defined.) */
-if (Meteor.users.find().count() === 0) {
-  if (Meteor.settings.defaultUsers) {
-    console.log('Creating default profiles');
-    Meteor.settings.defaultUsers.map(profile => addProfile(profile));
-  } else {
-    console.log('Cannot initialize the database. Please invoke meteor with a settings file.');
   }
 }
